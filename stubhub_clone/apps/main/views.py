c9aaa219f8feb44861bbfa3bd527d7b304d68e34
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-24 14:22:03 -0500

    added html snippet to homepage

--- stubhub_clone/apps/main/views.py
+++ stubhub_clone/apps/main/views.py
@@ -2,6 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,HttpResponse,reverse
+from  models import *
 
 def index(request):
-    return render(request, "main/index.html")
+    
+    primaryPerformer = "Chicago Cubs"
+    events = Event.objects.filter(performers__name=primaryPerformer)
+
+    context = {
+        'events':events,
+        'primaryPerformer': primaryPerformer
+    }
+    return render(request, "main/index.html", context)
