0a07e26c22f0112334eb2eccf4026815b4f341d1
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-24 10:36:39 -0500

    homepage template made

--- stubhub_clone/apps/main/views.py
+++ stubhub_clone/apps/main/views.py
@@ -4,6 +4,4 @@ from __future__ import unicode_literals
 from django.shortcuts import render,HttpResponse,reverse
 
 def index(request):
-    return HttpResponse("StubHUb Home")
-
-# Create your views here.
+    return render(request, "main/index.html")
