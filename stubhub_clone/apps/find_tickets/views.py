55ef98eb9d46164684698735f42aa121da0eb25d
MikeSully <sullyut@gmail.com>
2017-07-24 23:43:51 -0500

    Adding Search

--- stubhub_clone/apps/find_tickets/views.py
+++ stubhub_clone/apps/find_tickets/views.py
@@ -4,10 +4,31 @@ from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from datetime import *
 
 # Create your views here.
-def results(request):
+def process(request):
     if request.method !="POST":
         return redirect(reverse('main:index')) 
-    #search_query = request.POST['query']
-    search_events=Event.objects.filter(string_icontains="")
+    request.session['search_string'] = request.POST['searchInfo']
+    return redirect(reverse ('search:results'))
+
+def results(request):
+    search_string = request.session['search_string']
+    search_results = Event.objects.filter(title__icontains=search_string).order_by('start_time')
+    search_count = Event.objects.filter(title__icontains=search_string).count()
+    results=[]
+    for search_result in search_results:
+        curr_dict = {}
+        curr_dict['day']=search_result.start_time.strftime('%a')
+        curr_dict['time']=search_result.start_time.strftime('%I:%M %p')
+        curr_dict['title']= search_result.title
+        curr_dict['venue']= search_result.venue.title
+        curr_dict['date']=search_result.start_time.strftime('%b %d')
+        results.append(curr_dict)
+    context = {
+        'search_results':results,
+        'search_count':search_count
+    }
+    return render(request, 'find_tickets/results.html', context)
+    
